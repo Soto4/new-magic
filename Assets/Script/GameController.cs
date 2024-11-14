da22a47294f750c47f967b6f87a988aedc12268f
Dahlia Febriani <dahliafebriani57@gmail.com>
2024-11-15 01:10:21 +0700

    finish the gameplay mini game

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,24 +1,135 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class GameController : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite bgImage;
+    public Sprite[] puzzles;
+    public List<Sprite> gamePuzzles = new List<Sprite>();
     public List<Button> btns = new List<Button>();
+    private bool firstGuess, secondGuess;
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+    private int firstGuessIndex, secondGuessIndex;
+    private string firstGuesspuzzle, secondGuessPuzzle;
+
+    void Awake()
+    {
+        puzzles = Resources.LoadAll<Sprite>("Sprites");
+    }
+
 
     void Start()
     {
         GetButtons();
+        AddListener();
+        AddGamePuzzle();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
     }
 
     void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+        for (int i = 0; i < objects.Length; i++)
         {
-            GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
-            for (int i = 0; i < objects.Length; i++)
-            {
-                btns.Add(objects[i].GetComponent<Button>());
-            }
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
         }
-}
+    }
+    void AddGamePuzzle()
+    {
+        int looper = btns.Count;
+        int index = 0;
 
+        for (int i = 0; i < looper; i++)
+        {
+            if(index == looper/2)
+            {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+            index++;
+        }
+    }
+    void AddListener()
+    {
+        foreach (Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        if (!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstGuesspuzzle = gamePuzzles[firstGuessIndex].name;
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }
+        else if (!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            countGuesses++;
+            StartCoroutine(CheckIfThePuzzlesMatch());
+        }
+    }
+    IEnumerator CheckIfThePuzzlesMatch()
+    {
+        yield return new WaitForSeconds(1f);
+            if (firstGuesspuzzle == secondGuessPuzzle)
+            {
+            yield return new WaitForSeconds(.5f);
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            CheckIfTheGameIsFinish();
+            } else
+            {
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+            }
+            yield return new WaitForSeconds(.5f);
+        firstGuess = secondGuess = false;
+
+    }
+    void CheckIfTheGameIsFinish()
+    {
+        countCorrectGuesses++;
+        if (countCorrectGuesses == gameGuesses)
+        {
+            Debug.Log("You Won!");
+            Debug.Log("It Took You have " + countGuesses + " many Guesses to finish the game");
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = 0;  i < list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = UnityEngine.Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+   }
+
+
+    
+       
+       
+       
