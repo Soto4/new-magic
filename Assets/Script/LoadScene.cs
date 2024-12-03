4b74e0715f868ccc39e2ef71130de483604a2d6e
Dahlia Febriani <dahliafebriani57@gmail.com>
2024-12-03 16:19:33 +0700

    marge mini game and gameplay

--- Assets/Script/LoadScene.cs
+++ Assets/Script/LoadScene.cs
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SceneLoader : MonoBehaviour
-{
-    // Fungsi untuk memuat scene berdasarkan nama
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LoadScene : MonoBehaviour
 {
->>>>>>> menu,cutscene,gameplay
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-<<<<<<< HEAD
-    // Fungsi untuk keluar dari game
-    public void QuitGame()
-    {
-        Debug.Log("Keluar dari game!"); // Untuk debugging di editor
-        Application.Quit();
-    }
-=======
-
->>>>>>> menu,cutscene,gameplay
 }
